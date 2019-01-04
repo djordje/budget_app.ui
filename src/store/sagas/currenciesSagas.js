3b50cbe443cee5ea44d71177a4ab33e3f6160443
Djordje Kovacevic <djordje.bigjoe@gmail.com>
2019-01-04 17:56:16 +0100

    Update currenciesSagas to use generateProtectedApiCallSaga

--- src/store/sagas/currenciesSagas.js
+++ src/store/sagas/currenciesSagas.js
@@ -1,22 +1,21 @@
-import { call, select, put, takeEvery } from 'redux-saga/effects';
+import { takeEvery } from 'redux-saga/effects';
 import {
   FETCH_CURRENCIES_REQUEST,
   fetchCurrenciesSuccess,
   fetchCurrenciesFailure
 } from '../actions/currenciesActions';
 import { getPaginatedCurrencies } from '../../services/http/budgetAppAPI/requests/currencies';
-import { getApiToken } from '../selectors/apiTokenSelector';
+import { generateProtectedApiCallSaga } from '../utils/protectedApiCallSaga';
 
-export function* fetchCurrencies(action) {
-  const { page = 1, perPage = 10 } = action;
-  const { accessToken } = yield select(getApiToken);
-  try {
-    const response = yield call(getPaginatedCurrencies, accessToken, page, perPage);
-    yield put(fetchCurrenciesSuccess(response.data))
-  } catch (e) {
-    yield put(fetchCurrenciesFailure(e.message))
-  }
-}
+export const fetchCurrencies = generateProtectedApiCallSaga(
+  getPaginatedCurrencies,
+  action => ([
+    action.page,
+    action.perPage
+  ]),
+  fetchCurrenciesSuccess,
+  fetchCurrenciesFailure
+);
 
 export function* watchFetchCurrenciesRequest() {
   yield takeEvery(FETCH_CURRENCIES_REQUEST, fetchCurrencies);
--- src/store/sagas/currenciesSagas.test.js
+++ src/store/sagas/currenciesSagas.test.js
@@ -1,45 +1,8 @@
-import { call, put, select, takeEvery } from 'redux-saga/effects';
+import { takeEvery } from 'redux-saga/effects';
 import * as currenciesSagas from './currenciesSagas';
-import { getPaginatedCurrencies } from '../../services/http/budgetAppAPI/requests/currencies';
-import {
-  FETCH_CURRENCIES_REQUEST,
-  fetchCurrenciesSuccess,
-  fetchCurrenciesFailure
-} from '../actions/currenciesActions';
-import { getApiToken } from '../selectors/apiTokenSelector';
+import { FETCH_CURRENCIES_REQUEST } from '../actions/currenciesActions';
 
 describe('currenciesSagas', () => {
-  describe('fetchCurrencies', () => {
-    const generator = currenciesSagas.fetchCurrencies({});
-    const token = 'access-token-1';
-
-    it('is generator fn', () => {
-      expect(generator.value).toBeUndefined();
-      expect(generator.done).toBeFalsy();
-    });
-
-    it('selects apiToken.accessToken from the store', () => {
-      expect(generator.next().value).toEqual(
-        select(getApiToken)
-      );
-    });
-
-    it('calls api request', () => {
-      const apiToken = { accessToken: token };
-      expect(generator.next(apiToken).value).toEqual(call(getPaginatedCurrencies, token, 1, 10));
-    });
-
-    it('puts fetch currencies success action', () => {
-      const response = { status: 200, data: {} };
-      expect(generator.next(response).value).toEqual(put(fetchCurrenciesSuccess(response.data)));
-    });
-
-    it('puts fetch currencies failure action', () => {
-      const e = new Error('error');
-      expect(generator.throw(e).value).toEqual(put(fetchCurrenciesFailure(e.message)));
-    });
-  });
-
   describe('watchFetchCurrenciesRequest', () => {
     const generator = currenciesSagas.watchFetchCurrenciesRequest();
 
